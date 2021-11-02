8111271b94b126c50ddd98c5ce013786fbca9727
Christian Damian Cristofano <christiancristofano@gmail.com>
2021-11-02 00:38:49 -0300

    Agregar archivos de proyecto.

---
+++ PromocionesFravega.API/Data/Interfaces/ICatalogContext.cs
@@ -0,0 +1,10 @@
+﻿using PromocionesFravega.API.Entities;
+using MongoDB.Driver;
+
+namespace PromocionesFravega.API.Data.Interfaces
+{
+    public interface ICatalogContext
+    {
+        IMongoCollection<Product> Products { get; }
+    }
+}
